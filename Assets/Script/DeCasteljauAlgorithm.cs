652eadd007ff77f380da43bcff9a6170f3c4af58
Aedrinios <clementclerc2@gmail.com>
2021-05-14 12:19:58 +0200

    add:casteljau algorithm

---
+++ Assets/Script/DeCasteljauAlgorithm.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeCasteljauAlgorithm : MonoBehaviour
+{
+    public List<Vector3> BezierCurve(List<Vector3> controlPoints, float step)
+    {
+        List<Vector3> curvePoints = new List<Vector3>();
+        List<Vector3> barycentrePoints = new List<Vector3>();
+        for (float t = 0; t < 1; t += step)
+        {
+            barycentrePoints = controlPoints;
+            for (int i = 1; i < controlPoints.Count; i++)
+            {
+                for (int j = 0; j < controlPoints.Count - i; j++)
+                {
+                    barycentrePoints[j] = (1 - t) * controlPoints[j] + t * controlPoints[j + 1];
+                }
+            }
+            curvePoints.Add(barycentrePoints[0]);
+        }
+        return curvePoints;
+    }
+}
