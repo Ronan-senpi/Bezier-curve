0a43b6b290d64ab2e43878872b306ec100299f70
Ronan <r.dhersignerie@gmail.com>
2021-05-14 11:03:05 +0200

    add : get screen pos + files struct

---
+++ Assets/Script/GetScreenClickPosition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetScreenClickPosition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject go;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (go != null)
+            {
+                Instantiate(go, getWorldPos(), Quaternion.identity);
+            }
+        }
+    }
+    public Vector3 getWorldPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Debug.Log(mousePos);
+        mousePos.z = 10;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        Debug.Log(worldPosition);
+        return worldPosition;
+    }
+}
+
+
