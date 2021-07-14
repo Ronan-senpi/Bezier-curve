ac3687f9e48b08a8a5772ffeeb8e6e1e9041768e
Ronan <r.dhersignerie@gmail.com>
2021-07-14 16:12:35 +0200

    OrbitCamera control

---
+++ Assets/Script/OrbitCamera.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed = 5f;
+    [SerializeField]
+    float smoothFactor = 5f;
+
+    Vector3 offsetCam;
+    private void Start()
+    {
+        offsetCam = transform.position - Vector3.zero;
+        transform.LookAt(Vector3.zero);
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            offsetCam = camTurnAngle * offsetCam;
+            transform.position = Vector3.Slerp(transform.position, offsetCam, smoothFactor);
+            transform.LookAt(Vector3.zero);
+        }
+    }
+}
